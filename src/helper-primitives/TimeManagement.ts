8d185ed80108ca2f0bc6ab08bcd4f858909c04d9
Miłosz Gilga <gilgamilosz451@gmail.com>
2022-02-25 00:51:54 +0100

    create webapp time management class (with copy date method)

---
+++ src/helper-primitives/TimeManagement.ts
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2022 by MILOSZ GILGA <https://miloszgilga.pl>
+ *
+ * File name: TimeManagement.ts
+ * Last modified: 24/02/2022, 14:19
+ * Project name: digititles-imagine
+ *
+ * Licensed under the MIT license; you may not use this file except in compliance with the License.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * THE ABOVE COPYRIGHT NOTICE AND THIS PERMISSION NOTICE SHALL BE INCLUDED IN ALL
+ * COPIES OR SUBSTANTIAL PORTIONS OF THE SOFTWARE.
+ */
+
+class TimeManagement {
+
+    public static generateCopyDate(): string {
+        const date = new Date().getFullYear();
+        return date === 2020 ? String(date) : `${2020}-${date}`;
+    };
+
+}
+
+export default TimeManagement;
