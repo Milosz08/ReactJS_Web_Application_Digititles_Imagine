30920d9ca17b1981722bd98e8faf0369deda5fc5
Miłosz Gilga <gilgamilosz451@gmail.com>
2022-02-25 00:46:36 +0100

    create custom hook responsible for managing header title animations on all subpages

---
+++ src/hooks/header-with-menu/useMainHeaderTitleAnimations.ts
@@ -0,0 +1,92 @@
+/*
+ * Copyright (c) 2022 by MILOSZ GILGA <https://miloszgilga.pl>
+ *
+ * File name: useMainHeaderTitleAnimations.ts
+ * Last modified: 23/02/2022, 18:19
+ * Project name: digititles-imagine
+ *
+ * Licensed under the MIT license; you may not use this file except in compliance with the License.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * THE ABOVE COPYRIGHT NOTICE AND THIS PERMISSION NOTICE SHALL BE INCLUDED IN ALL
+ * COPIES OR SUBSTANTIAL PORTIONS OF THE SOFTWARE.
+ */
+
+import * as React from 'react';
+import { useEffect, useLayoutEffect, useRef } from 'react';
+import { useSelector } from 'react-redux';
+import { useLocation } from 'react-router';
+
+import useDidMount from '../reusable/useDidMount';
+import { Gsap } from '../../helper-primitives/GsapAnimations';
+
+import { RootState } from '../../redux/store';
+import { InitStateDOMtypes } from '../../redux/redux-dom-manipulate/initialState';
+import { AnimationDirections, AnimationStages } from '../../redux/redux-dom-manipulate/types';
+
+/**
+ * Custom hook responsible for managing header title animations on all subpages. Include manage visibility
+ * on load, on scroll event and while user open/closed menu.
+ *
+ * @return { [ React.MutableRefObject<any>, boolean ] } - first is the referential object, second is the menu open flag.
+ */
+const useMainHeaderTitleAnimations = (): [ React.MutableRefObject<any>, boolean ] => {
+
+    const { ifMenuOpen, currScrollPos }: InitStateDOMtypes = useSelector((state: RootState) => state.reduxReducerDOM);
+
+    const { SHOW, HIDE } = AnimationStages;
+    const { LEFT, RIGHT } = AnimationDirections;
+
+    const isMount = useDidMount();
+    const headerTitleRef = useRef();
+    const { pathname } = useLocation();
+
+    // litening change route and disable visibility on patch '/getting-started'
+    useLayoutEffect((): void => {
+        if (!isMount) {
+            window.scrollTo(0, 0);
+            if (pathname === '/getting-started') {
+                Gsap.gsapBasicAnimations(headerTitleRef, { dir: RIGHT, interpos: 20, visible: HIDE, duration: .6 });
+            } else {
+                Gsap.gsapBasicAnimations(headerTitleRef, { dir: RIGHT, visible: SHOW, duration: .6 });
+            }
+        }
+    }, [ pathname, headerTitleRef ]);
+
+    // change visibility on scroll event
+    useLayoutEffect((): void => {
+        if (!isMount && pathname !== '/getting-started') {
+            if (currScrollPos > 30) {
+                Gsap.gsapBasicAnimations(headerTitleRef, { dir: LEFT, interpos: 15, visible: HIDE });
+            } else {
+                Gsap.gsapBasicAnimations(headerTitleRef, { dir: LEFT, visible: SHOW });
+            }
+        }
+    }, [ currScrollPos ]);
+
+    // on menu open/closed animations
+    useLayoutEffect((): void => {
+        if (!isMount) {
+            if (ifMenuOpen) {
+                Gsap.gsapBasicAnimations(headerTitleRef, { dir: LEFT, interpos: 10, visible: HIDE });
+            } else if (currScrollPos <= 30) {
+                Gsap.gsapBasicAnimations(headerTitleRef, { dir: LEFT, visible: SHOW, delay: .4 });
+            }
+        }
+    }, [ ifMenuOpen ]);
+
+    // load animation on first component mount
+    useEffect((): void => {
+        if (isMount && pathname !== '/getting-started') {
+            Gsap.gsapBasicAnimations(headerTitleRef, { dir: LEFT, visible: SHOW, delay: .6 });
+        }
+    }, [ isMount ]);
+
+    return [ headerTitleRef, ifMenuOpen ];
+};
+
+export default useMainHeaderTitleAnimations;
