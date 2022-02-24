68c86dd7058c3cc33cb59ccdf834a891979fbd1e
Miłosz Gilga <gilgamilosz451@gmail.com>
2022-02-25 00:55:56 +0100

    create NavigationScrollTop component (with styles)

---
+++ src/components/navigation-scroll-top/NavigationScrollTop.styles.ts
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2022 by MILOSZ GILGA <https://miloszgilga.pl>
+ *
+ * File name: NavigationScrollTop.styles.ts
+ * Last modified: 23/02/2022, 20:46
+ * Project name: digititles-imagine
+ *
+ * Licensed under the MIT license; you may not use this file except in compliance with the License.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * THE ABOVE COPYRIGHT NOTICE AND THIS PERMISSION NOTICE SHALL BE INCLUDED IN ALL
+ * COPIES OR SUBSTANTIAL PORTIONS OF THE SOFTWARE.
+ */
+
+import styled from 'styled-components';
+
+import { button_rs } from '../../styles/reset.styles';
+import { HideElementOnLoad } from '../../styles/mixins.styles';
+
+export const NavigationScrollTopContainer = styled(button_rs)<{ $ifFixed: boolean }>`
+    position: ${({ $ifFixed }) => $ifFixed ? 'fixed' : 'absolute'};
+    display: flex;
+    align-items: center;
+    font-size: 1.3rem;
+    font-weight: 500;
+    color: var(--grayDark);
+    transition: .2s;
+    background-color: transparent;
+    bottom: 150px;
+    height: 80px;
+    left: 40px;
+    transform-origin: left;
+    transform: rotate(90deg);
+    ${HideElementOnLoad()};
+    :hover {
+        color: var(--cyanDark);
+    }
+    @media only screen and (max-width: 760px) {
+        display: none !important;
+    }
+`;
+
+export const NavigationScrollTopProgressBar = styled.div`
+    position: relative;
+    width: 4px;
+    height: 100%;
+    background-color: var(--grayLight);
+    margin-left: 20px;
+`;
+
+export const NavigationScrollTopProgressBarActive = styled.div.attrs<{ $percentageFill: number }>(props => ({
+    style: { height: `${props.$percentageFill}%` }
+}))<{ $percentageFill: number }>`
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    background-color: var(--cyanDark);
+`;
---
+++ src/components/navigation-scroll-top/NavigationScrollTop.tsx
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2022 by MILOSZ GILGA <https://miloszgilga.pl>
+ *
+ * File name: NavigationScrollTop.tsx
+ * Last modified: 23/02/2022, 20:45
+ * Project name: digititles-imagine
+ *
+ * Licensed under the MIT license; you may not use this file except in compliance with the License.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * THE ABOVE COPYRIGHT NOTICE AND THIS PERMISSION NOTICE SHALL BE INCLUDED IN ALL
+ * COPIES OR SUBSTANTIAL PORTIONS OF THE SOFTWARE.
+ */
+
+import * as React from 'react';
+import { useSelector } from 'react-redux';
+
+import { Gsap } from '../../helper-primitives/GsapAnimations';
+import useChangeVisibilityBasedMark from '../../hooks/navigation/useChangeVisibilityBasedMark';
+
+import { RootState } from '../../redux/store';
+import { AnimationVisibility } from '../../redux/redux-dom-manipulate/types';
+import { InitStateDOMtypes } from '../../redux/redux-dom-manipulate/initialState';
+
+import {
+    NavigationScrollTopContainer, NavigationScrollTopProgressBar, NavigationScrollTopProgressBarActive
+} from './NavigationScrollTop.styles';
+
+
+const NavigationScrollTop: React.FC = (): JSX.Element => {
+
+    const { scrollPercentage, ifFixed }: InitStateDOMtypes = useSelector((state: RootState) => state.reduxReducerDOM);
+
+    const [ navigationRef ] = useChangeVisibilityBasedMark({
+        reversed: true, animActivate: 500, visibleType: AnimationVisibility.FLEX, disableMediaQueryValue: 760
+    });
+
+    return (
+        <NavigationScrollTopContainer
+            onClick = {(): void => Gsap.scrollIntoContext(document.documentElement)}
+            ref = {navigationRef}
+            $ifFixed = {ifFixed}
+        >
+            Scroll to Top
+            <NavigationScrollTopProgressBar>
+                <NavigationScrollTopProgressBarActive
+                    $percentageFill = {scrollPercentage}
+                />
+            </NavigationScrollTopProgressBar>
+        </NavigationScrollTopContainer>
+    );
+};
+
+export default NavigationScrollTop;
