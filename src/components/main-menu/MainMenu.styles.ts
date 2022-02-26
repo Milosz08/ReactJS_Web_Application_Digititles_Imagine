549070336481574ffe4a8e2ba1407712aa832c27
Miłosz Gilga <gilgamilosz451@gmail.com>
2022-02-26 01:56:11 +0100

    initialised main menu component (with subcomponents)

---
+++ src/components/main-menu/MainMenu.styles.ts
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2022 by MILOSZ GILGA <https://miloszgilga.pl>
+ *
+ * File name: MainMenu.styles.ts
+ * Last modified: 25/02/2022, 01:04
+ * Project name: digititles-imagine
+ *
+ * Licensed under the MIT license; you may not use this file except in compliance with the License.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * THE ABOVE COPYRIGHT NOTICE AND THIS PERMISSION NOTICE SHALL BE INCLUDED IN ALL
+ * COPIES OR SUBSTANTIAL PORTIONS OF THE SOFTWARE.
+ */
+
+import styled from 'styled-components';
+
+export const MainMenuContainer = styled.main<{ $ifActive: boolean }>`
+    position: fixed;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    width: 100%;
+    height: 100vh;
+    background-color: var(--cyanDark);
+    transform: translateY(${({ $ifActive }) => $ifActive ? 0 : '-100vh'});
+    opacity: ${({ $ifActive }) => $ifActive ? 1 : 0};
+    transition: .6s all cubic-bezier(.56, 0, .46, 1);
+    padding: 0 50px;
+    z-index: 4;
+    overflow-y: scroll;
+    @media only screen and (max-width: 760px) {
+        display: block;
+        padding: 80px 30px 30px;
+    }
+`;
+
+export const MainMenuSingleSectionContainer = styled.section`
+    display: flex;
+    flex-direction: column;
+    border: 1px solid black;
+    height: 400px;
+    @media only screen and (max-width: 760px) {
+        width: 100%;
+        margin: 20px 0;
+    }
+`;
+
+export const MainMenuCenterSingleSectionContainer = styled(MainMenuSingleSectionContainer)`
+    @media only screen and (max-width: 1030px) {
+        display: none;
+    }
+    @media only screen and (max-width: 760px) {
+        display: flex;
+    }
+`;
---
+++ src/components/main-menu/subcomponents/LeftContent.tsx
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2022 by MILOSZ GILGA <https://miloszgilga.pl>
+ *
+ * File name: LeftContent.tsx
+ * Last modified: 25/02/2022, 01:04
+ * Project name: digititles-imagine
+ *
+ * Licensed under the MIT license; you may not use this file except in compliance with the License.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * THE ABOVE COPYRIGHT NOTICE AND THIS PERMISSION NOTICE SHALL BE INCLUDED IN ALL
+ * COPIES OR SUBSTANTIAL PORTIONS OF THE SOFTWARE.
+ */
+
+import * as React from 'react';
+
+import DelayRouteLinkHOC from '../../../high-order-components/DelayRouteLinkHOC';
+import { MainMenuSingleSectionContainer } from '../MainMenu.styles';
+
+
+const LeftContent: React.FC = (): JSX.Element => {
+
+
+
+    return (
+        <MainMenuSingleSectionContainer>
+            <DelayRouteLinkHOC to = '/getting-started' delay = {1000} forceScroll = {true}>
+                getting started
+            </DelayRouteLinkHOC>
+            <DelayRouteLinkHOC to = '/projects' delay = {1000} forceScroll = {true}>
+                projects
+            </DelayRouteLinkHOC>
+            <DelayRouteLinkHOC to = '/services' delay = {1000} forceScroll = {true}>
+                services
+            </DelayRouteLinkHOC>
+        </MainMenuSingleSectionContainer>
+    );
+};
+
+export default LeftContent;
