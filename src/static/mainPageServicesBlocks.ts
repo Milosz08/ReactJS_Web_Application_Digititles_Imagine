8341bf6e29f0791ab7de11a105d9316fc78a3634
Miłosz Gilga <gilgamilosz451@gmail.com>
2022-02-27 18:49:07 +0100

    create services section static content

---
+++ src/static/mainPageServicesBlocks.ts
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2022 by MILOSZ GILGA <https://miloszgilga.pl>
+ *
+ * File name: mainPageServicesBlocks.ts
+ * Last modified: 27/02/2022, 00:45
+ * Project name: digititles-imagine
+ *
+ * Licensed under the MIT license; you may not use this file except in compliance with the License.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * THE ABOVE COPYRIGHT NOTICE AND THIS PERMISSION NOTICE SHALL BE INCLUDED IN ALL
+ * COPIES OR SUBSTANTIAL PORTIONS OF THE SOFTWARE.
+ */
+
+export interface MainPageServicesBlocksTypes {
+    name: string;
+    description: string;
+    activeImage: string;
+}
+
+export const mainPageServicesBlocks: MainPageServicesBlocksTypes[] = [
+    {
+        name: 'main credits',
+        description: 'The opening credits, which are indirectly a promotion for the entire movie.',
+        activeImage: 'maincredits',
+    },
+    {
+        name: 'end credits & end cards',
+        description: 'End credits/cards, paying tribute to all the people who worked on the movie.',
+        activeImage: 'endcredits',
+    },
+    {
+        name: 'subtitles',
+        description: 'Translation of dialogue questions in feature and documentary films in the popular formats ' +
+            'like .txt and .srt',
+        activeImage: 'subtitles',
+    },
+];
