35d0ac35af6c1117436a5f92c9567449c7ff2e1c
Miłosz Gilga <gilgamilosz451@gmail.com>
2022-02-28 21:32:17 +0100

    initialised AllProjectsList component with all styles

---
+++ src/components/all-projects-list/AllProjectsList.styles.ts
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2022 by MILOSZ GILGA <https://miloszgilga.pl>
+ *
+ * File name: AllProjectsList.styles.ts
+ * Last modified: 28/02/2022, 14:37
+ * Project name: digititles-imagine
+ *
+ * Licensed under the MIT license; you may not use this file except in compliance with the License.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * THE ABOVE COPYRIGHT NOTICE AND THIS PERMISSION NOTICE SHALL BE INCLUDED IN ALL
+ * COPIES OR SUBSTANTIAL PORTIONS OF THE SOFTWARE.
+ */
+
+import styled from 'styled-components';
+
+export const AllProjectsListContainer = styled.section`
+    width: 100%;
+    padding: 50px 0;
+`;
