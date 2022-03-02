ca89b0f359ee9454a976123cec943ae33ea65cde
Miłosz Gilga <gilgamilosz451@gmail.com>
2022-03-03 00:19:14 +0100

    implement some custom styles for menu single containers and child elements

--- src/components/main-menu/MainMenu.styles.ts
+++ src/components/main-menu/MainMenu.styles.ts
@@ -30,7 +30,7 @@ export const MainMenuContainer = styled.main<{ $ifActive: boolean }>`
     transform: translateY(${({ $ifActive }) => $ifActive ? 0 : '-100vh'});
     opacity: ${({ $ifActive }) => $ifActive ? 1 : 0};
     transition: .6s all cubic-bezier(.56, 0, .46, 1);
-    padding: 0 50px;
+    padding: 50px 0;
     z-index: 4;
     overflow-y: auto;
     @media only screen and (max-width: 760px) {
@@ -42,20 +42,43 @@ export const MainMenuContainer = styled.main<{ $ifActive: boolean }>`
 export const MainMenuSingleSectionContainer = styled.section`
     display: flex;
     flex-direction: column;
-    height: 400px;
+    flex-basis: calc(100% / 3);
     max-width: 420px;
+    margin: 0 30px;
     @media only screen and (max-width: 760px) {
         width: 100%;
         margin: 20px 0;
     }
 `;
 
+export const MainMenuLeftSingleSectionContainer = styled(MainMenuSingleSectionContainer)`
+    min-width: 360px;
+    @media only screen and (max-width: 720px) {
+        min-width: 100%;
+    }
+`;
+
 export const MainMenuCenterSingleSectionContainer = styled(MainMenuSingleSectionContainer)`
-    @media only screen and (max-width: 1030px) {
+    flex-grow: 1;
+    max-width: 420px;
+`;
+
+export const MainMenuRightSingleSectionContainer = styled(MainMenuSingleSectionContainer)`
+    max-width: 300px;
+    @media only screen and (max-width: 1160px) {
         display: none;
     }
     @media only screen and (max-width: 760px) {
         display: flex;
+    }
+`;
+
+export const MainMenuRightEmailSectionContainer = styled.div`
+    margin: 10px 0 50px;
+    font-size: var(--paragraphFontSize);
+    font-weight: 500;
+    @media only screen and (max-width: 500px) {
+        margin: 10px 0;
     }
 `;
 
@@ -72,9 +95,12 @@ export const MainMenuHeaderElement = styled.h3<{ $ifMarginTop?: boolean }>`
         content: '';
         bottom : -10px;
         left: 0;
-        width: 40%;
+        width: 50px;
         height: 2px;
         background-color: var(--cyanLight);
+    }
+    @media only screen and (max-width: 760px) {
+        margin: 40px 0 30px;
     }
 `;
 
@@ -85,12 +111,39 @@ export const MainMenuMainLinks = styled(delay_link_rs)`
     text-transform: capitalize;
     transition: .2s;
     margin-bottom: 10px;
+    width: fit-content;
     :hover {
         color: var(--blackDark);
+    }
+    @media only screen and (max-width: 500px) {
+        font-size: 2.5rem;
     }
 `;
 
 export const MainMenuParagraphElement = styled.p`
     font-size: 1.2rem;
     color: var(--whiteClean);
+`;
+
+export const MainMenuProjectsList = styled.ul`
+    list-style-type: none;
+`;
+
+export const MainMenuSingleProjectInListContainer = styled.li``;
+
+export const MainMenuProjectSingleElement = styled(delay_link_rs)`
+    color: var(--whiteClean);
+    width: fit-content;
+    font-size: 2rem;
+    margin: 3px 0;
+    font-weight: 500;
+    transition: .2s;
+    line-height: 1.3;
+    :hover {
+        color: var(--blackDark);
+    }
+    @media only screen and (max-width: 500px) {
+        font-size: 1.6rem;
+        line-height: 1.7;
+    }
 `;
