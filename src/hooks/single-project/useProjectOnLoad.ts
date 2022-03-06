4a676bfcb107ea8ebc6229c022efb22636a703ae
Miłosz Gilga <gilgamilosz451@gmail.com>
2022-03-06 03:46:16 +0100

    create custom hook responsible for finding and inserting data of single project.

---
+++ src/hooks/single-project/useProjectOnLoad.ts
@@ -0,0 +1,74 @@
+/*
+ * Copyright (c) 2022 by MILOSZ GILGA <https://miloszgilga.pl>
+ *
+ * File name: useProjectOnLoad.ts
+ * Last modified: 06/03/2022, 03:11
+ * Project name: digititles-imagine
+ *
+ * Licensed under the MIT license; you may not use this file except in compliance with the License.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * THE ABOVE COPYRIGHT NOTICE AND THIS PERMISSION NOTICE SHALL BE INCLUDED IN ALL
+ * COPIES OR SUBSTANTIAL PORTIONS OF THE SOFTWARE.
+ */
+
+import { useLayoutEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
+import { RoutingPaths } from '../../static/appRouting';
+import useChangePageTitle from '../reusable/useChangePageTitle';
+import { SubpagesContent, SubpagesContentKeys, SubpagesMainContentTypes } from '../../static/subpagesMainContent';
+
+import { RootState } from '../../redux/store';
+import { ProjectTypes } from '../../redux/redux-api-thunk/elementTypes';
+import { InitStateAPItypes } from '../../redux/redux-api-thunk/initialState';
+
+/**
+ * Custom hook responsible for finding and inserting data of single project. When hook not find matched project
+ * to saved ID value, then move to 404 page.
+ *
+ * @param projectTitle { string } - project title (from URL params).
+ * @return { [ ProjectTypes, SubpagesMainContentTypes, string ] } - first: foundProject, second: project main page
+ *         content, third: project main photo element.
+ */
+const useProjectOnLoad = (projectTitle: string): [ ProjectTypes, SubpagesMainContentTypes, string ] => {
+
+    const { projects, projectsPhotos, status }: InitStateAPItypes = useSelector((state: RootState) => state.reduxReducerAPI);
+
+    const foundProject = projects.find(project => project.projectPath === projectTitle)!;
+    SubpagesContent[SubpagesContentKeys.PROJECT].dotColor = foundProject?.projectColours.strongForeground;
+
+    const navigate = useNavigate();
+
+    const [ findingProject, setFindingProject ] = useState<ProjectTypes>();
+    const [ photo, setPhoto ] = useState<string>();
+    const [ content, setContent ] = useState<SubpagesMainContentTypes>(SubpagesContent[SubpagesContentKeys.PROJECT]);
+
+    useChangePageTitle(findingProject ? findingProject.title : 'Loading...', false);
+
+    // On load component added project values
+    useLayoutEffect(() => {
+        if (Object.keys(status).every(loading => !status[loading])) {
+            const foundProject = projects.find(project => project.projectPath === projectTitle)!;
+            if (foundProject) {
+                const mainImage = projectsPhotos
+                    .find(project => project.projectId === foundProject?.id)!
+                    .projectImages.find(image => image.name.toLocaleLowerCase().includes('main'))!;
+                setPhoto(mainImage.url);
+                setFindingProject(foundProject);
+                setContent(prevState => ({ ...prevState, title: foundProject.title }));
+            } else {
+                navigate({ pathname: RoutingPaths.ERROR });
+            }
+        }
+    }, [ navigate, projectTitle, projects, projectsPhotos, status ]);
+
+    return [ findingProject!, content, photo! ];
+};
+
+export default useProjectOnLoad;
