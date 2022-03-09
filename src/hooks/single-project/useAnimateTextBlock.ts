c3eda0c0302f02e7e4b98dca434e1823644bc826
Miłosz Gilga <gilgamilosz451@gmail.com>
2022-03-09 03:04:46 +0100

    create custom hook responsible for include enter/outer animation for all children of root element

---
+++ src/hooks/single-project/useAnimateTextBlock.ts
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) 2022 by MILOSZ GILGA <https://miloszgilga.pl>
+ *
+ * File name: useAnimateTextBlock.ts
+ * Last modified: 08/03/2022, 15:56
+ * Project name: digititles-imagine
+ *
+ * Licensed under the MIT license; you may not use this file except in compliance with the License.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * THE ABOVE COPYRIGHT NOTICE AND THIS PERMISSION NOTICE SHALL BE INCLUDED IN ALL
+ * COPIES OR SUBSTANTIAL PORTIONS OF THE SOFTWARE.
+ */
+
+import * as React from 'react';
+import { useCallback, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { gsap } from 'gsap';
+
+import { RootState } from '../../redux/store';
+import { InitStateDOMtypes } from '../../redux/redux-dom-manipulate/initialState';
+import { ReduxDOMActions, SectionKey } from '../../redux/redux-dom-manipulate/actions';
+import { AllSections, ProjectSections, ServicesSections } from '../../redux/redux-dom-manipulate/types';
+
+interface HookProps {
+    key: AllSections;
+    value: SectionKey;
+}
+
+/**
+ * Custom hook responsible for include enter/outer animation for all children of root element (passing
+ * in params). Also changing active section state (in redux store).
+ *
+ * @param parentElement { React.MutableRefObject<any> } - referential parent container.
+ * @param section { { key: AllSections, value: SectionKey } } - activate section label element (by key and value).
+ */
+const useAnimateTextBlock = (parentElement: React.MutableRefObject<any>, { key, value }: HookProps): null => {
+
+    const { browserX }: InitStateDOMtypes = useSelector((state: RootState) => state.reduxReducerDOM);
+    const dispatcher = useDispatch();
+
+    const invokeHandlerOnEnter = useCallback(() => {
+        dispatcher(ReduxDOMActions.changeActiveSection(key, value));
+    }, [ dispatcher, key, value ]);
+
+    const invokeHandlerOnLeave = useCallback(() => {
+        const { TITLE, DESCRIPTION } = ProjectSections;
+        const { END_CREDITS, MAIN_CREDITS } = ServicesSections;
+        if (value === END_CREDITS || value === DESCRIPTION) {
+            dispatcher(ReduxDOMActions.changeActiveSection(key, key === AllSections.SERVICES ? MAIN_CREDITS : TITLE));
+        }
+    }, [ dispatcher, key, value ]);
+
+    useEffect(() => {
+        gsap.from(parentElement.current.children, {
+            x: -50, autoAlpha: 0, stagger: .2, scrollTrigger: {
+                trigger: parentElement.current,
+                start: 'center bottom',
+                end: 'bottom bottom',
+                onEnter: invokeHandlerOnEnter,
+                onEnterBack: invokeHandlerOnEnter,
+                onLeaveBack: invokeHandlerOnLeave,
+            },
+        });
+    }, [ browserX, invokeHandlerOnEnter, invokeHandlerOnLeave, parentElement ]);
+
+    return null;
+};
+
+export default useAnimateTextBlock;
