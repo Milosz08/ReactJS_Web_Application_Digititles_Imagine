e5b89b8a55ca8332baccc8c808a336a198378590
Miłosz Gilga <gilgamilosz451@gmail.com>
2022-03-15 02:21:54 +0100

    create custom hook responsible for forcing page hard reload after every change browser size

---
+++ src/hooks/reusable/useForcePageReload.ts
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2022 by MILOSZ GILGA <https://miloszgilga.pl>
+ *
+ * File name: useForcePageReload.ts
+ * Last modified: 15/03/2022, 01:05
+ * Project name: digititles-imagine
+ *
+ * Licensed under the MIT license; you may not use this file except in compliance with the License.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * THE ABOVE COPYRIGHT NOTICE AND THIS PERMISSION NOTICE SHALL BE INCLUDED IN ALL
+ * COPIES OR SUBSTANTIAL PORTIONS OF THE SOFTWARE.
+ */
+
+import { useEffect } from 'react';
+
+/**
+ * Custom hook responsible for forcing page hard reload after every change browser size.
+ */
+const useForcePageReload = (): null => {
+
+    useEffect(() => {
+        window.addEventListener('resize', () => {
+            window.location.href = window.location.href;
+        });
+    }, []);
+
+    return null;
+};
+
+export default useForcePageReload;
