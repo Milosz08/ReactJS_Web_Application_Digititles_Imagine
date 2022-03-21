c890c8e21222f422150026ffa16811ed7dc44085
Miłosz Gilga <gilgamilosz451@gmail.com>
2022-03-21 01:52:39 +0100

    create UniversalPasswordInput component (with styles)

---
+++ src/components/universal-password-input/UniversalPasswordInput.styles.ts
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2022 by MILOSZ GILGA <https://miloszgilga.pl>
+ *
+ * File name: UniversalPasswordInput.styles.ts
+ * Last modified: 20/03/2022, 20:50
+ * Project name: digititles-imagine
+ *
+ * Licensed under the MIT license; you may not use this file except in compliance with the License.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * THE ABOVE COPYRIGHT NOTICE AND THIS PERMISSION NOTICE SHALL BE INCLUDED IN ALL
+ * COPIES OR SUBSTANTIAL PORTIONS OF THE SOFTWARE.
+ */
+
+import styled from 'styled-components';
+
+import { button_rs, input_rs } from '../../styles/reset.styles';
+import { InputWithTextarea } from '../../styles/mixins.styles';
+
+export const UniversalPasswordInputLabel = styled.label`
+    position: relative;
+    width: 100%;
+`;
+
+export const UniversalPasswordInputElement = styled(input_rs)<{ $ifError: boolean }>`
+    ${({ $ifError }) => InputWithTextarea({ $ifError })};
+    margin-bottom: 20px;
+`;
+
+export const UniversalPasswordButtonChangeVisibility = styled(button_rs)<{ $ifError: boolean }>`
+    position: absolute;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    right: 15px;
+    width: 40px;
+    height: 40px;
+    color: var(--${({ $ifError }) => $ifError ? 'redLight' : 'grayDarker'});
+    top: 50%;
+    transform: translateY(-50%);
+    font-size: 1.8rem;
+`;
---
+++ src/components/universal-password-input/UniversalPasswordInput.tsx
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) 2022 by MILOSZ GILGA <https://miloszgilga.pl>
+ *
+ * File name: UniversalPasswordInput.tsx
+ * Last modified: 20/03/2022, 20:50
+ * Project name: digititles-imagine
+ *
+ * Licensed under the MIT license; you may not use this file except in compliance with the License.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * THE ABOVE COPYRIGHT NOTICE AND THIS PERMISSION NOTICE SHALL BE INCLUDED IN ALL
+ * COPIES OR SUBSTANTIAL PORTIONS OF THE SOFTWARE.
+ */
+
+import * as React from 'react';
+import { Dispatch, SetStateAction, useState } from 'react';
+
+import { AiOutlineEye, AiOutlineEyeInvisible } from 'react-icons/ai';
+
+import {
+    UniversalPasswordButtonChangeVisibility, UniversalPasswordInputElement, UniversalPasswordInputLabel
+} from './UniversalPasswordInput.styles';
+
+
+interface PropsProvider {
+    placeholder: string;
+    ifProtected?: boolean;
+    ifError: boolean;
+    elementRef: React.MutableRefObject<any>;
+}
+
+const UniversalPasswordInput: React.FC<PropsProvider> = ({ placeholder, ifProtected, ifError, elementRef }): JSX.Element => {
+
+    const [ isHidden, setIsHidden ] = useState<boolean>(true);
+
+    const changeVisibility = (): void => {
+        if (Boolean(elementRef.current.value)) {
+            setIsHidden(prevState => !prevState)
+        }
+    };
+
+    return (
+        <UniversalPasswordInputLabel
+            htmlFor = {placeholder}
+        >
+            <UniversalPasswordInputElement
+                type = {ifProtected ? isHidden ? 'password' : 'text' : 'text'}
+                placeholder = {placeholder}
+                $ifError = {ifError}
+                id = {placeholder}
+                ref = {elementRef}
+            />
+            {ifProtected && <UniversalPasswordButtonChangeVisibility
+                onClick = {changeVisibility}
+                $ifError = {ifError}
+                type = 'button'
+            >
+                {isHidden ? <AiOutlineEyeInvisible/> : <AiOutlineEye/>}
+            </UniversalPasswordButtonChangeVisibility>}
+        </UniversalPasswordInputLabel>
+    );
+};
+
+export default UniversalPasswordInput;
