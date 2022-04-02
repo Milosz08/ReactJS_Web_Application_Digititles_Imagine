4f0aec8d0af32911f5e226a21342e5e5cfe8a0a4
Miłosz Gilga <gilgamilosz451@gmail.com>
2022-04-03 00:16:45 +0200

    prevent create cookies when they already exist

--- src/hooks/login-cms-page/useCmsLoginAndValidateData.ts
+++ src/hooks/login-cms-page/useCmsLoginAndValidateData.ts
@@ -41,7 +41,7 @@ interface HookProps {
  */
 const useCmsLoginAndValidateData = (): HookProps => {
 
-    const { setCookie } = useContext<Partial<AllCookiesTypes>>(AllCookiesContext);
+    const { cookie, setCookie } = useContext<Partial<AllCookiesTypes>>(AllCookiesContext);
     const [ errors, setErrors ] = useState<{ loginE: boolean, passwordE: boolean }>({ loginE: false, passwordE: false });
 
     const { elRefs } = useMultipleRefs(2);
@@ -53,8 +53,10 @@ const useCmsLoginAndValidateData = (): HookProps => {
         const senderObject = { username: login.current.value, password: password.current.value };
         axiosInstance.post(`${JavaApiEndpoints.AUTHENTICATIONS}/validate`, senderObject)
             .then(res => {
-                setCookie!(AllCookies.BEARER_TOKEN, res.data.bearerToken);
-                setCookie!(AllCookies.CMS_SESSION, res.data.authLevel);
+                if (!cookie![AllCookies.BEARER_TOKEN] && !cookie![AllCookies.CMS_SESSION]) {
+                    setCookie!(AllCookies.BEARER_TOKEN, res.data.bearerToken);
+                    setCookie!(AllCookies.CMS_SESSION, res.data.authLevel);
+                }
                 dispatcher(ReduxAPIActions.changeSessionInfo(true, res.data.authLevel, res.data.bearerToken));
             })
             .catch(err => {
